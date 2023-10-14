626cc221aab6e70675a2f84217c5b9cae440650d
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-10-14 15:42:28 +0100

    Add simple event handling

--- app/src/main/java/ie/setu/bin_there_app/BinThereActivity.kt
+++ app/src/main/java/ie/setu/bin_there_app/BinThereActivity.kt
@@ -3,14 +3,22 @@ package ie.setu.bin_there_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.ajalt.timberkt.Timber
+import ie.setu.bin_there_app.databinding.ActivityBinthereBinding
 import timber.log.Timber.i
 
 class BinThereActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBinthereBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_binthere)
+
+        binding = ActivityBinthereBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
         i("BinThere Activity started..")
+
+        binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
+        }
     }
 }
