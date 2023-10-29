f1eadb4924826ac53a9b10ba59aec79f2f23c8d4
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-10-29 20:51:03 +0000

    Refactor POI Adapter

--- app/src/main/java/ie/setu/bin_there_app/activities/PoiListActivity.kt
+++ app/src/main/java/ie/setu/bin_there_app/activities/PoiListActivity.kt
@@ -4,18 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ie.setu.bin_there_app.R
+import ie.setu.bin_there_app.adapters.PoiAdapter
 import ie.setu.bin_there_app.databinding.ActivityPoiListBinding
-import ie.setu.bin_there_app.databinding.CardPoiBinding
 import ie.setu.bin_there_app.main.MainApp
-import ie.setu.bin_there_app.models.PoiModel
+
 
 class PoiListActivity : AppCompatActivity() {
 
@@ -61,29 +58,4 @@ class PoiListActivity : AppCompatActivity() {
                 notifyItemRangeChanged(0,app.pois.size)
             }
         }
-}
-
-class PoiAdapter constructor(private var pois: List<PoiModel>) :
-        RecyclerView.Adapter<PoiAdapter.MainHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardPoiBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val poi = pois[holder.adapterPosition]
-        holder.bind(poi)
-    }
-
-    override fun getItemCount(): Int = pois.size
-
-    class MainHolder(private val binding : CardPoiBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(poi: PoiModel) {
-            binding.poiTitle.text = poi.title
-            binding.description.text = poi.description
-        }
-    }
 }
---
+++ app/src/main/java/ie/setu/bin_there_app/adapters/PoiAdapter.kt
@@ -0,0 +1,33 @@
+package ie.setu.bin_there_app.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ie.setu.bin_there_app.databinding.CardPoiBinding
+import ie.setu.bin_there_app.models.PoiModel
+
+class PoiAdapter constructor(private var pois: List<PoiModel>) :
+    RecyclerView.Adapter<PoiAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardPoiBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val poi = pois[holder.adapterPosition]
+        holder.bind(poi)
+    }
+
+    override fun getItemCount(): Int = pois.size
+
+    class MainHolder(private val binding : CardPoiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(poi: PoiModel) {
+            binding.poiTitle.text = poi.title
+            binding.description.text = poi.description
+        }
+    }
+}
