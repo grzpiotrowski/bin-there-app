e4143185dc2f73ff6fcc669956554e0024b548fe
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-10-30 15:12:20 +0000

    Add File helper

--- app/src/main/java/ie/setu/bin_there_app/main/MainApp.kt
+++ app/src/main/java/ie/setu/bin_there_app/main/MainApp.kt
@@ -3,15 +3,17 @@ package ie.setu.bin_there_app.main
 import android.app.Application
 import com.github.ajalt.timberkt.Timber
 import ie.setu.bin_there_app.models.PoiMemStore
+import ie.setu.bin_there_app.models.PoiStore
 import timber.log.Timber.i
 
 class MainApp : Application () {
 
-    val pois = PoiMemStore()
+    lateinit var pois: PoiStore
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        pois = PoiMemStore()
         i("BinThere started")
     }
 }
