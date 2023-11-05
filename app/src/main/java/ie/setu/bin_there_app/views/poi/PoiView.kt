660696d59f7b83baf209b49197b4fc99cbc31593
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-11-05 20:50:26 +0000

    Update POI View with new fields

--- app/src/main/java/ie/setu/bin_there_app/views/poi/PoiView.kt
+++ app/src/main/java/ie/setu/bin_there_app/views/poi/PoiView.kt
@@ -4,12 +4,15 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
 import ie.setu.bin_there_app.R
 import ie.setu.bin_there_app.databinding.ActivityPoiBinding
 import ie.setu.bin_there_app.models.poi.PoiModel
+import ie.setu.bin_there_app.models.poi.PoiType
 import timber.log.Timber.i
 
 class PoiView : AppCompatActivity() {
@@ -20,7 +23,6 @@ class PoiView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         binding = ActivityPoiBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -41,6 +43,13 @@ class PoiView : AppCompatActivity() {
 
         binding.gpsButton.setOnClickListener {
             presenter.getCurrentLocation()
+        }
+
+        binding.spinnerPoiType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                updateViewForPoiType(PoiType.values()[position])
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
         binding.btnAdd.setOnClickListener {
@@ -90,5 +99,26 @@ class PoiView : AppCompatActivity() {
             .load(image)
             .into(binding.poiImage)
         binding.chooseImage.setText(R.string.change_poi_image)
+    }
+
+    private fun updateViewForPoiType(poiType: PoiType) {
+        when (poiType) {
+            PoiType.LITTER -> {
+                binding.spinnerLitterType.visibility = View.VISIBLE
+                binding.labelLitterType.visibility = View.VISIBLE
+                binding.spinnerBinStatus.visibility = View.GONE
+                binding.labelBinStatus.visibility = View.GONE
+                binding.spinnerBinType.visibility = View.GONE
+                binding.labelBinType.visibility = View.GONE
+            }
+            PoiType.BIN -> {
+                binding.spinnerLitterType.visibility = View.GONE
+                binding.labelLitterType.visibility = View.GONE
+                binding.spinnerBinStatus.visibility = View.VISIBLE
+                binding.labelBinStatus.visibility = View.VISIBLE
+                binding.spinnerBinType.visibility = View.VISIBLE
+                binding.labelBinType.visibility = View.VISIBLE
+            }
+        }
     }
 }
