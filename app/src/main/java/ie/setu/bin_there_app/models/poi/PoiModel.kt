f144d960cb66c2439066622796ac562999f4135f
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-11-05 20:49:57 +0000

    Enhance POI model

--- app/src/main/java/ie/setu/bin_there_app/models/poi/PoiModel.kt
+++ app/src/main/java/ie/setu/bin_there_app/models/poi/PoiModel.kt
@@ -5,14 +5,37 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class PoiModel(var id: Long = 0,
-                    var userId: Long = 0,
-                    var title: String = "",
-                    var description: String = "",
-                    var image: Uri = Uri.EMPTY,
-                    var location: Location = Location()
+data class PoiModel(
+    var id: Long = 0,
+    var userId: Long = 0,
+    var poiType: PoiType = PoiType.LITTER,
+    var title: String = "",
+    var description: String = "",
+    var image: Uri = Uri.EMPTY,
+    var location: Location = Location(),
+    var dateReported: Long = System.currentTimeMillis(),
+    var isCleanedUp: Boolean = false,
+    var litterType: LitterType? = null,
+    var binStatus: BinStatus? = null,
+    var binType: BinType? = null,
+
 ) : Parcelable
 
+enum class PoiType {
+    LITTER, BIN
+}
+
+enum class LitterType {
+    GENERAL, PAPER, PLASTIC, ORGANIC, METAL, OTHER
+}
+
+enum class BinStatus {
+    NORMAL, OVERFLOWING, SUGGESTED_NEW_LOCATION
+}
+
+enum class BinType {
+    GENERAL, RECYCLABLES, ORGANIC, ELECTRONIC, GLASS, OTHER
+}
 
 @Parcelize
 data class Location(var lat: Double = 0.0,
