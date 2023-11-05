7cc0037e309cabe16163829617a7ffecc13cbdcf
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-11-05 04:27:58 +0000

    User model and signup implemented

--- app/src/main/java/ie/setu/bin_there_app/views/signup/SignupView.kt
+++ app/src/main/java/ie/setu/bin_there_app/views/signup/SignupView.kt
@@ -1,4 +1,45 @@
 package ie.setu.bin_there_app.views.signup
 
-class SignupView {
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ie.setu.bin_there_app.R
+import ie.setu.bin_there_app.databinding.ActivitySignupBinding
+import ie.setu.bin_there_app.models.user.UserJSONStore
+import ie.setu.bin_there_app.models.user.UserModel
+import timber.log.Timber.i
+
+class SignupView : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var presenter: SignupPresenter
+
+    var user = UserModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        i("Signup activity started.")
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        presenter = SignupPresenter(this, UserJSONStore(this))
+
+        binding.signupButton.setOnClickListener {
+            i("Bttn click")
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            presenter.onSignupClicked(email, password)
+        }
+    }
+
+    fun onSignupSuccess() {
+        Toast.makeText(this, "Signed up successfully", Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun onSignupError(error: String) {
+        Toast.makeText(this, "Signup error", Toast.LENGTH_SHORT).show()
+    }
+
 }
---
+++ app/src/main/java/ie/setu/bin_there_app/views/welcome/WelcomeView.kt
@@ -0,0 +1,29 @@
+package ie.setu.bin_there_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ie.setu.bin_there_app.databinding.ActivityWelcomeBinding
+import ie.setu.bin_there_app.views.signup.SignupView
+import timber.log.Timber.i
+
+class WelcomeView : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            //val loginIntent = Intent(this, LoginActivity::class.java)
+            //startActivity(loginIntent)
+        }
+
+        binding.signupButton.setOnClickListener {
+            val signupIntent = Intent(this, SignupView::class.java)
+            startActivity(signupIntent)
+        }
+    }
+}
