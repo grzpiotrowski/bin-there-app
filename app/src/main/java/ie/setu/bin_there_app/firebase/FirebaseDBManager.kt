3b0f9bbb4fb432f4271f57057d840d76e9bf28ef
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-12-20 21:38:14 +0000

    Add Firebase DB

---
+++ app/src/main/java/ie/setu/bin_there_app/firebase/FirebaseDBManager.kt
@@ -0,0 +1,90 @@
+package ie.setu.bin_there_app.firebase
+
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import ie.setu.bin_there_app.models.PoiModel
+import ie.setu.bin_there_app.models.PoiStore
+import timber.log.Timber
+
+object FirebaseDBManager : PoiStore {
+
+    var database: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    override fun findAll(poisList: MutableLiveData<List<PoiModel>>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(userid: String, poisList: MutableLiveData<List<PoiModel>>) {
+
+        database.child("user-pois").child(userid)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    Timber.i("Firebase POI error : ${error.message}")
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val localList = ArrayList<PoiModel>()
+                    val children = snapshot.children
+                    children.forEach {
+                        val poi = it.getValue(PoiModel::class.java)
+                        localList.add(poi!!)
+                    }
+                    database.child("user-pois").child(userid)
+                        .removeEventListener(this)
+
+                    poisList.value = localList
+                }
+            })
+    }
+
+    override fun findById(userid: String, poiid: String, poi: MutableLiveData<PoiModel>) {
+
+        database.child("user-pois").child(userid)
+            .child(poiid).get().addOnSuccessListener {
+                poi.value = it.getValue(PoiModel::class.java)
+                Timber.i("firebase Got value ${it.value}")
+            }.addOnFailureListener{
+                Timber.e("firebase Error getting data $it")
+            }
+    }
+
+    override fun create(firebaseUser: MutableLiveData<FirebaseUser>, poi: PoiModel) {
+        Timber.i("Firebase DB Reference : $database")
+
+        val uid = firebaseUser.value!!.uid
+        val key = database.child("pois").push().key
+        if (key == null) {
+            Timber.i("Firebase Error : Key Empty")
+            return
+        }
+        poi.id = key
+        val poiValues = poi.toMap()
+
+        val childAdd = HashMap<String, Any>()
+        childAdd["/pois/$key"] = poiValues
+        childAdd["/user-pois/$uid/$key"] = poiValues
+
+        database.updateChildren(childAdd)
+    }
+
+    override fun delete(userid: String, poiid: String) {
+
+        val childDelete : MutableMap<String, Any?> = HashMap()
+        childDelete["/pois/$poiid"] = null
+        childDelete["/user-pois/$userid/$poiid"] = null
+
+        database.updateChildren(childDelete)
+    }
+
+    override fun update(userid: String, poiid: String, poi: PoiModel) {
+
+        val poiValues = poi.toMap()
+
+        val childUpdate : MutableMap<String, Any?> = HashMap()
+        childUpdate["pois/$poiid"] = poiValues
+        childUpdate["user-pois/$userid/$poiid"] = poiValues
+
+        database.updateChildren(childUpdate)
+    }
+}
--- app/src/main/java/ie/setu/bin_there_app/models/PoiModel.kt
+++ app/src/main/java/ie/setu/bin_there_app/models/PoiModel.kt
@@ -2,12 +2,14 @@ package ie.setu.bin_there_app.models
 
 import android.net.Uri
 import android.os.Parcelable
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
 import kotlinx.parcelize.Parcelize
 
-
+@IgnoreExtraProperties
 @Parcelize
 data class PoiModel(
-    var id: Long = 0,
+    var id: String? = "",
     var userId: Long = 0,
     var poiType: PoiType = PoiType.LITTER,
     var title: String = "",
@@ -20,14 +22,31 @@ data class PoiModel(
     var binStatus: BinStatus? = null,
     var binType: BinType? = null,
 
-    ) : Parcelable
+    ) : Parcelable {
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "id" to id,
+            "userId" to userId,
+            "poiType" to poiType.name,
+            "title" to title,
+            "description" to description,
+            "location" to location.toMap(),
+            "dateReported" to dateReported,
+            "isCleanedUp" to isCleanedUp,
+            "litterType" to litterType?.name,
+            "binStatus" to binStatus?.name,
+            "binType" to binType?.name
+        )
+    }
+}
 
 enum class PoiType {
     LITTER, BIN
 }
 
 enum class LitterType {
-    GENERAL, PAPER, PLASTIC, ORGANIC, METAL, OTHER, HEHEHE
+    GENERAL, PAPER, PLASTIC, ORGANIC, METAL, OTHER
 }
 
 enum class BinStatus {
@@ -41,4 +60,12 @@ enum class BinType {
 @Parcelize
 data class Location(var lat: Double = 0.0,
                     var lng: Double = 0.0,
-                    var zoom: Float = 0f) : Parcelable
+                    var zoom: Float = 0f) : Parcelable {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "lat" to lat,
+            "lng" to lng,
+            "zoom" to zoom
+        )
+    }
+}
